dadd8fd4075c8b581da32d3baee6f6aaec21bff5
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-10 13:49:49 -0700

    slight refactor

--- esbuild.config.go
+++ esbuild.config.go
@@ -1,85 +1,88 @@
+// READ FIRST: https://github.com/evanw/esbuild/issues/39
+
 package main
 
-import (
-  "log"
-  "os"
-
-  "github.com/evanw/esbuild/pkg/api"
-)
+// import (
+// 	"log"
+// 	//   "os"
+// 	//   "github.com/evanw/esbuild/pkg/api"
+// )
 
 func main() {
-  modulalResult := api.Build(api.BuildOptions{
-    EntryPoints:       []string{"src/form.mjs", "src/main.mjs", "src/modules.js"},
-    Bundle:            true,
-    Format:            api.FormatESModule,
-    MinifyIdentifiers: true,
-    MinifySyntax:      true,
-    MinifyWhitespace:  true,
-    Outdir:            "assets",
-    Sourcemap:         api.SourceMapLinked,
-    Splitting:         true,
-    // Watch: &api.WatchMode{
-    //   OnRebuild: func(result api.BuildResult) {
-    //     if len(result.Errors) > 0 {
-    //       fmt.Printf("watch build failed: %d errors\n", len(result.Errors))
-    //     } else {
-    //       fmt.Printf("watch build succeeded: %d warnings\n", len(result.Warnings))
-    //     }
-    //   },
-    // },
-    Engines: []api.Engine{
-      {Name: api.EngineChrome, Version: "88"},
-      {Name: api.EngineFirefox, Version: "86"},
-      {Name: api.EngineSafari, Version: "14"},
-      {Name: api.EngineEdge, Version: "16"},
-      {Name: api.EngineNode, Version: "12.19.0"},
-    },
-    Write: true,
-  })
+	// 	// This may be is working, but use npm script to run the esbuild
 
-  if len(modulalResult.Errors) > 0 {
-    for _, e := range modulalResult.Errors {
-      log.Println(e)
-    }
-    os.Exit(1)
-  }
+	// 	//   modulalResult := api.Build(api.BuildOptions{
+	// 	//     EntryPoints:       []string{"src/form.mjs", "src/main.mjs", "src/modules.js"},
+	// 	//     Bundle:            true,
+	// 	//     Format:            api.FormatESModule,
+	// 	//     MinifyIdentifiers: true,
+	// 	//     MinifySyntax:      true,
+	// 	//     MinifyWhitespace:  true,
+	// 	//     Outdir:            "assets",
+	// 	//     Sourcemap:         api.SourceMapLinked,
+	// 	//     Splitting:         true,
+	// 	//     // Watch: &api.WatchMode{
+	// 	//     //   OnRebuild: func(result api.BuildResult) {
+	// 	//     //     if len(result.Errors) > 0 {
+	// 	//     //       fmt.Printf("watch build failed: %d errors\n", len(result.Errors))
+	// 	//     //     } else {
+	// 	//     //       fmt.Printf("watch build succeeded: %d warnings\n", len(result.Warnings))
+	// 	//     //     }
+	// 	//     //   },
+	// 	//     // },
+	// 	//     Engines: []api.Engine{
+	// 	//       {Name: api.EngineChrome, Version: "88"},
+	// 	//       {Name: api.EngineFirefox, Version: "86"},
+	// 	//       {Name: api.EngineSafari, Version: "14"},
+	// 	//       {Name: api.EngineEdge, Version: "16"},
+	// 	//       {Name: api.EngineNode, Version: "12.19.0"},
+	// 	//     },
+	// 	//     Write: true,
+	// 	//   })
 
-  // modulalResult.Stop()
+	// 	//   if len(modulalResult.Errors) > 0 {
+	// 	//     for _, e := range modulalResult.Errors {
+	// 	//       log.Println(e)
+	// 	//     }
+	// 	//     os.Exit(1)
+	// 	//   }
 
-  fallbackResult := api.Build(api.BuildOptions{
-    EntryPoints:       []string{"src/form.mjs", "src/main.mjs", "src/modules.js"},
-    Bundle:            true,
-    Format:            api.FormatIIFE,
-    MinifyIdentifiers: true,
-    MinifySyntax:      true,
-    MinifyWhitespace:  true,
-    Outdir:            "assets/fallback",
-    Sourcemap:         api.SourceMapLinked,
-    // Watch: &api.WatchMode{
-    //   OnRebuild: func(result api.BuildResult) {
-    //     if len(result.Errors) > 0 {
-    //       fmt.Printf("watch build failed: %d errors\n", len(result.Errors))
-    //     } else {
-    //       fmt.Printf("watch build succeeded: %d warnings\n", len(result.Warnings))
-    //     }
-    //   },
-    // },
-    Engines: []api.Engine{
-      {Name: api.EngineChrome, Version: "88"},
-      {Name: api.EngineFirefox, Version: "86"},
-      {Name: api.EngineSafari, Version: "14"},
-      {Name: api.EngineEdge, Version: "16"},
-      {Name: api.EngineNode, Version: "12.19.0"},
-    },
-    Write: true,
-  })
+	// 	//   // modulalResult.Stop()
 
-  if len(fallbackResult.Errors) > 0 {
-    for _, e := range fallbackResult.Errors {
-      log.Println(e)
-    }
-    os.Exit(1)
-  }
+	// 	//   fallbackResult := api.Build(api.BuildOptions{
+	// 	//     EntryPoints:       []string{"src/form.mjs", "src/main.mjs", "src/modules.js"},
+	// 	//     Bundle:            true,
+	// 	//     Format:            api.FormatIIFE,
+	// 	//     MinifyIdentifiers: true,
+	// 	//     MinifySyntax:      true,
+	// 	//     MinifyWhitespace:  true,
+	// 	//     Outdir:            "assets/fallback",
+	// 	//     Sourcemap:         api.SourceMapLinked,
+	// 	//     // Watch: &api.WatchMode{
+	// 	//     //   OnRebuild: func(result api.BuildResult) {
+	// 	//     //     if len(result.Errors) > 0 {
+	// 	//     //       fmt.Printf("watch build failed: %d errors\n", len(result.Errors))
+	// 	//     //     } else {
+	// 	//     //       fmt.Printf("watch build succeeded: %d warnings\n", len(result.Warnings))
+	// 	//     //     }
+	// 	//     //   },
+	// 	//     // },
+	// 	//     Engines: []api.Engine{
+	// 	//       {Name: api.EngineChrome, Version: "88"},
+	// 	//       {Name: api.EngineFirefox, Version: "86"},
+	// 	//       {Name: api.EngineSafari, Version: "14"},
+	// 	//       {Name: api.EngineEdge, Version: "16"},
+	// 	//       {Name: api.EngineNode, Version: "12.19.0"},
+	// 	//     },
+	// 	//     Write: true,
+	// 	//   })
 
-  // fallbackResult.Stop()
+	// //   if len(fallbackResult.Errors) > 0 {
+	// //     for _, e := range fallbackResult.Errors {
+	// //       log.Println(e)
+	// //     }
+	// //     os.Exit(1)
+	// //   }
+	// log.Println("Success")
+	// //   // fallbackResult.Stop()
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,65 @@
-const path = require("path");
-const webpack = require("webpack");
-const isDevelopment = process.env.NODE_ENV !== "production";
-const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+// Webpack is deprecated for the sake of esbuild
 
-module.exports = {
-    mode: isDevelopment ? "development" : "production",
-    entry: {
-        main: "./src/main.js",
-        form: "./src/form.mjs",
-        vue: "./src/vue.js",
-        greensock: "./src/greensock.js"
-    },
-    output: {
-        path: path.resolve(__dirname, "assets"),
-        filename: "[name].js"
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            autoprefixer: {},
-            moment: "moment"
-        }),
-        // To strip all locales except “en”
-        // new MomentLocalesPlugin(),
-        // Or: To strip all locales except “en” and “ru”
-        // (“en” is built into Moment and can’t be removed)
-        new MomentLocalesPlugin({
-            localesToKeep: ["ru"]
-        })
-    ],
-    module: {
-        rules: [{
-            test: /\.s(a|c)ss$/,
-            exclude: /\.module.(s(a|c)ss)$/,
-            use: [{
-                loader: "file-loader",
-                options: {
-                    name: "[name].css"
-                }
-            }, {
-                loader: "extract-loader"
-            }, {
-                loader: "css-loader?-url"
-            }, {
-                loader: "postcss-loader"
-            }, {
-                loader: "sass-loader"
-            }]
-        }],
-        exports: [{
-            node: 'empty',
-        }]
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".scss", ".css"]
-    },
-    externals: {
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-        Modernizr: 'modernizr'
-    }
-};
+// const path = require("path");
+// const webpack = require("webpack");
+// const isDevelopment = process.env.NODE_ENV !== "production";
+// const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+
+// module.exports = {
+//     mode: isDevelopment ? "development" : "production",
+//     entry: {
+//         main: "./src/main.js",
+//         form: "./src/form.mjs",
+//         vue: "./src/vue.js",
+//         greensock: "./src/greensock.js"
+//     },
+//     output: {
+//         path: path.resolve(__dirname, "assets"),
+//         filename: "[name].js"
+//     },
+//     plugins: [
+//         new webpack.ProvidePlugin({
+//             autoprefixer: {},
+//             moment: "moment"
+//         }),
+//         // To strip all locales except “en”
+//         // new MomentLocalesPlugin(),
+//         // Or: To strip all locales except “en” and “ru”
+//         // (“en” is built into Moment and can’t be removed)
+//         new MomentLocalesPlugin({
+//             localesToKeep: ["ru"]
+//         })
+//     ],
+//     module: {
+//         rules: [{
+//             test: /\.s(a|c)ss$/,
+//             exclude: /\.module.(s(a|c)ss)$/,
+//             use: [{
+//                 loader: "file-loader",
+//                 options: {
+//                     name: "[name].css"
+//                 }
+//             }, {
+//                 loader: "extract-loader"
+//             }, {
+//                 loader: "css-loader?-url"
+//             }, {
+//                 loader: "postcss-loader"
+//             }, {
+//                 loader: "sass-loader"
+//             }]
+//         }],
+//         exports: [{
+//             node: 'empty',
+//         }]
+//     },
+//     resolve: {
+//         extensions: [".js", ".jsx", ".scss", ".css"]
+//     },
+//     externals: {
+//         $: 'jquery',
+//         jQuery: 'jquery',
+//         'window.jQuery': 'jquery',
+//         Modernizr: 'modernizr'
+//     }
+// };
